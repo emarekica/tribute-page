7013ec41739b1ace2c242558ca3ae52bb54258e6
emarekica <emarekica@gmail.com>
2022-02-25 14:06:52 +0100

    fix(CSS): tablet, padding, margins, JS
    - adjust media queries for tablet version
    - adjust padding & margins
    - adjust img, padding and margins for desktop version
    - start JS "modal window" for imgs

---
+++ abramovic.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//// 1.
+// select the elements you need
+// store the selections into variables
+// use them repeatedly
+
+//// 2.
+// work with existing HTML classes
+// write classess you will use from HTML
+// event handler onClick triggers show/hide of modal windows = photos
